0043df3cabe9d956ab3776425cb4424eeae590cd
Lewis <88146791+lafexx@users.noreply.github.com>
2023-07-08 07:14:06 +1000

    Fetches raw json response

--- Cyclone/MainPage.xaml.cs
+++ Cyclone/MainPage.xaml.cs
@@ -5,5 +5,12 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		GetWeather("Sydney");
+	}
+
+	public static async Task GetWeather(string Location)
+	{
+		var json = await WeatherApi.weatherApiClient.GetStringAsync($"{Location}&aqi=no");
+	
 	}
 }
---
+++ Cyclone/WeatherApi.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Cyclone
+{
+	public static class WeatherApi
+	{
+		public static HttpClient weatherApiClient;
+
+		static WeatherApi()
+		{
+			weatherApiClient = new HttpClient
+			{
+				BaseAddress = new Uri("https://api.weatherapi.com/v1/current.json?key=APIKeyHere&q=")
+			}; // initialising new client
+
+        }
+	}
+}
+
