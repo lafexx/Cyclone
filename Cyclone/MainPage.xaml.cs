ab47b8aef7e5d65c2a70095c6d03de64781c4b88
Lewis <88146791+lafexx@users.noreply.github.com>
2023-07-08 10:04:11 +1000

    Added more to adv weather info

--- Cyclone/MainPage.xaml.cs
+++ Cyclone/MainPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class MainPage : ContentPage
 
 	public async void GetWeather(string Location)
 	{
-		string json = await WeatherApi.client.GetStringAsync($"https://api.weatherapi.com/v1/current.json?key=APIKEYHERE&q={Location}&aqi=no");
+		string json = await WeatherApi.client.GetStringAsync($"https://api.weatherapi.com/v1/current.json?key=c0074171cefb4be1b20180157230707&q={Location}&aqi=no");
         WeatherModel weather = JsonConvert.DeserializeObject<WeatherModel>(json);
         weatherCity.Text = $"{weather.location.name}";
         weatherRegionAndCountry.Text = $"{weather.location.region}, {weather.location.country}";
@@ -25,8 +25,8 @@ public partial class MainPage : ContentPage
 		weatherWindDegree.Text = $"Wind Degree: {weather.current.wind_degree}º";
 		weatherWindDirection.Text = $"Wind Direction: {weather.current.wind_dir}";
 		weatherPrecip.Text = $"Precipitation: {weather.current.precip_mm}mm";
-		weatherHumidity.Text = $"{weather.current.humidity}";
-		weatherUV.Text = $"{weather.current.uv}";
-		weatherVisibility.Text = $"{weather.current.vis_km}km";
+		weatherHumidity.Text = $"Humidity: {weather.current.humidity}";
+		weatherUV.Text = $"UV: {weather.current.uv}";
+		weatherVisibility.Text = $"Visibility: {weather.current.vis_km}km";
     }
 }
