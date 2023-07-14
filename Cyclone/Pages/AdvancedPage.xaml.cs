cdb15750bffb80cee65267d95381caff579e4ade
Lewis <88146791+lafexx@users.noreply.github.com>
2023-07-15 07:03:03 +1000

    Added weather null check in the adv pg

--- Cyclone/Pages/AdvancedPage.xaml.cs
+++ Cyclone/Pages/AdvancedPage.xaml.cs
@@ -12,15 +12,18 @@ public partial class AdvancedPage : ContentPage
     {
         WeatherModel weather = WeatherApi.weather;
 
-        // Setting the advanced weather information
-        weatherAirPressure.Text = $"Air Pressure: {weather.current.pressure_in}";
-        weatherWindSpeed.Text = $"Wind Speed: {weather.current.wind_kph}km/h";
-        weatherWindDegree.Text = $"Wind Degree: {weather.current.wind_degree}º";
-        weatherWindDirection.Text = $"Wind Direction: {weather.current.wind_dir}";
-        weatherPrecip.Text = $"Precipitation: {weather.current.precip_mm}mm";
-        weatherHumidity.Text = $"Humidity: {weather.current.humidity}";
-        weatherUV.Text = $"UV: {weather.current.uv}";
-        weatherVisibility.Text = $"Visibility: {weather.current.vis_km}km";
+        if (weather != null)
+        {
+            // Setting the advanced weather information
+            weatherAirPressure.Text = $"Air Pressure: {weather.current.pressure_in}";
+            weatherWindSpeed.Text = $"Wind Speed: {weather.current.wind_kph}km/h";
+            weatherWindDegree.Text = $"Wind Degree: {weather.current.wind_degree}ï¿½";
+            weatherWindDirection.Text = $"Wind Direction: {weather.current.wind_dir}";
+            weatherPrecip.Text = $"Precipitation: {weather.current.precip_mm}mm";
+            weatherHumidity.Text = $"Humidity: {weather.current.humidity}";
+            weatherUV.Text = $"UV: {weather.current.uv}";
+            weatherVisibility.Text = $"Visibility: {weather.current.vis_km}km";
+        }
     }
 
     void RedirectToHome(System.Object sender, System.EventArgs e)
